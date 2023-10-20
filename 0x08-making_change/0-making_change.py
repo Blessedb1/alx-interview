4e5bf57b5ffe59d93d522237ba53aca4cc0b7389
Blessing Bassey Etim <109361841+Blessedb1@users.noreply.github.com>
2023-10-20 05:48:05 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-""" Contains makeChange function"""
+""" Creating a makeChange function"""
 
 
 def makeChange(coins, total):
     """
-    Returns: fewest number of coins needed to meet total
+    Outputs: fewest number of coins needed to meet total
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
